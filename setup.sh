b84c8f9b0a1378493a865a6fcf88577e83978991
Seiya Takei <SeiyaTakei@SeiyaTakei.local>
2018-01-04 13:10:18 +0900

    config.json, serviceファイルのセットアップをこれ1つでできるよう変更

--- setup.sh
+++ setup.sh
@@ -1,12 +1,36 @@
-echo "*----- modify service files -----*" 
-tmp_path=`pwd`
-iksm_proxy_path=`echo $tmp_path/main.py` 
+echo "*---------------------------+"
+echo "|      Setup iksmproxy      |"
+echo "+---------------------------+"
+echo ""
+
+echo "*----- splatnet2statink -----*"
+echo "Enter splatnet2statink directory:"
+read splatnet2statink_path
+current_path=`cat config.json | grep splatnet2statink | awk '{print $2}' | sed 's/"//g'`
+sed "s%$current_path%$splatnet2statink_path%g" config.json > tmp_config.json
+mv tmp_config.json config.json
+echo "config.json:"
+cat config.json
+echo ""
+
+echo "*----- modify service files -----*"
+iksm_proxy_path=`pwd`
 echo "iksmproxy path: "$iksm_proxy_path
-splatnet2statink_path=`cat config.json | grep splatnet2statink | awk '{print $2}' | sed 's/"//g'`
 echo "splatnet2statink path: "$splatnet2statink_path
+echo ""
+
 cd service/
+
 sed "s%^WorkingDirectory.*%WorkingDirectory=$iksm_proxy_path%g" iksmproxy.service > tmp_iksmproxy.service
 mv tmp_iksmproxy.service iksmproxy.service
+echo "=== iksmproxy.service ==="
+cat iksmproxy.service
+echo "========================="
+echo ""
 
 sed "s%^WorkingDirectory.*%WorkingDirectory=$splatnet2statink_path%g" splatnet2statink.service > tmp_splatnet2statink.service
 mv tmp_splatnet2statink.service splatnet2statink.service
+echo "=== splatnet2statink.service ==="
+cat splatnet2statink.service
+echo "================================"
+
